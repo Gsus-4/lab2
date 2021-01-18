4a3b2ee9b5376b323badd36d84739abecb2587f6
Jesús Vicente Martínez <jevima.biker@gmail.com>
2021-01-18 02:26:33 +0100

    Modificados los .sh que se han usado para obtener los .fastq

--- file-cut.sh
+++ file-cut.sh
@@ -5,13 +5,26 @@
 #Cortado obligatorio (primer 20avo del fichero): file-cut.sh 1
 #Cortado avanzado (líneas que son múltiplo de 20): file-cut.sh 2
 
+#Los archivos de salida se guardan en una carpeta al efecto, que es creada si no existe
 
-if [ $1 == 2 ]; then
-	for (( s=1; s <= 4; s++ )); do
-		awk 'NR%20 == 0' Sample$s.fastq >> sample$s
 
-	done
-fi
+for (( s=1; s <= 4; s++ )); do
+	if [ $1 == 1 ]; then
+		mkdir -p obligatorio
+		l=($(wc -l < Sample$s.fastq))
+		head -n $(( $l/20 )) Sample$s.fastq >> ./obligatorio/Sample$s.fastq
+	fi
+
+	if [ $1 == 2 ]; then
+		mkdir -p avanzado
+		awk 'NR%20 == 0' Sample$s.fastq >> ./avanzado/Sample$s.fastq
+	fi
+done
+
+
+
+
+
 	
 
 
